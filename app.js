c1515a689b8f3aee34cac6616208665ca1797f24
TheTeaRex <chancalan@gmail.com>
2015-11-18 08:41:30 +0000

    will add http:// if none was given

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var Client = require('node-rest-client').Client;
 var request = require('request');
 var http = require('http');
+var S = require('string');
 
 var app = express();
 app.use(express.bodyParser());
 app.use("/images", express.static(__dirname + '/images'));
 
 var endpoint = "http://52.11.127.220"
+
 
 var page = function( req, res, state ) {
     var body = fs.readFileSync('./urlShortener.html');
@@ -18,6 +20,9 @@ var page = function( req, res, state ) {
     var msg = "This is the current state of this app: " + state + "<br>";
     if (state == "has-url"){
         var longurl = req.body.longurl;
+        if ( ! (S(longurl).startsWith("http://") || S(longurl).startsWith("https://"))){
+            longurl = "http://" + longurl;
+        }
         var shorturl;
         getShortUrl(longurl, function(shorturl){
         msg = msg + "Your Long URL: " + longurl + "<br>";
@@ -57,8 +62,11 @@ var handle_post = function (req, res) {
     console.log( "Post: " + "Submitted URL: " +  req.body.longurl + "\n" ) ;
     var longurl = "" + req.body.longurl ;
     //NEED TO VALIDATE LONG URL
-    //....
-    //....
+    if ( ! (S(longurl).startsWith("http://") || S(longurl).startsWith("https://"))){
+        longurl = "http://" + longurl;
+    }
+    console.log("longurl: "+longurl);
+
     if (longurl.trim() == "") {
         page( req, res, "no-url" ) ;
     }
