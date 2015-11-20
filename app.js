b0b8669c7829cddea7d96a1eb46011bfa0dffb16
TheTeaRex <chancalan@gmail.com>
2015-11-20 05:36:04 +0000

    changed the endpoint

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.bodyParser());
 app.use("/images", express.static(__dirname + '/images'));
 
-var endpoint = "https://52.11.127.220"
+var endpoint = "http://52.11.127.220";//"https://cpserver-dev.elasticbeanstalk.com/"
 
 
 var page = function( req, res, state ) {
